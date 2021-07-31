1db7e37b53353c32ab109a6fa26095e7c54c9487
MSProgrammerPy <None>
2021-07-31 19:34:13 +0430

    Form LoginForm ijad shod.

--- account/forms.py
+++ account/forms.py
@@ -1,1 +1,13 @@
+from django.utils.translation import gettext_lazy as _
 from django import forms
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={"class": "form-control", "placeholder": _("username")}),
+        label=_("username")
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={"class": "form-control", "placeholder": _("password")}),
+        label=_("password")
+    )
