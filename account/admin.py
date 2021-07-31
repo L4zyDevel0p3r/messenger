1064ae8983da6de5bfcd2b406dca6b2b01d7c973
MSProgrammerPy <None>
2021-07-31 15:00:49 +0430

    Class CustomUserAdmin ijad shod. Model 'User' dar admin register shod.

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,16 @@
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
+from .models import User
+
 
 # Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (_("Extra fields"), {"fields": ("picture",)}),
+    )
+
+    class Meta:
+        model = User
