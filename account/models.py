b3c6f88c0eb14ee97f3392756557ba039e1d36a3
MSProgrammerPy <None>
2021-07-31 13:38:10 +0430

    Function upload_user_pic ijad shod.

--- account/models.py
+++ account/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from uuid import uuid4
 import os
 
 
@@ -7,6 +8,13 @@ def get_filename_ext(filepath):
     basename = os.path.basename(filepath)
     name, ext = os.path.splitext(basename)
     return name, ext
+
+
+def upload_user_pic(instance, file):
+    name, ext = get_filename_ext(file)
+    random_str = str(uuid4()).split("-")[0]
+    final_name = f"profile_pictures/{instance.username}/{random_str}{ext}"
+    return final_name
 
 
 # Create your models here.
