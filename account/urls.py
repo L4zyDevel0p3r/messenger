383489881924158cca23e550f9573b656fa31909
MSProgrammerPy <None>
2021-07-31 19:49:47 +0430

    Path 'logout' be urlpatterns ezafe shod.

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,9 @@
-from .views import login_page
+from .views import login_page, logout_view
 from django.urls import path
 
 app_name = "Account"
 
 urlpatterns = [
     path('login', login_page, name="Login"),
+    path('logout', logout_view, name="Logout"),
 ]
