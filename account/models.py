5bffc352403f6ecd836a620c42ac5ff6f5450a78
MSProgrammerPy <None>
2021-07-31 13:32:30 +0430

    Function get_filename_ext ijad shod.

--- account/models.py
+++ account/models.py
@@ -1,5 +1,12 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+import os
+
+
+def get_filename_ext(filepath):
+    basename = os.path.basename(filepath)
+    name, ext = os.path.splitext(basename)
+    return name, ext
 
 
 # Create your models here.
