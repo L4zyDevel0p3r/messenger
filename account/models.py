43e80c0aa6e81d3d9250f01255281868b2ab025b
MSProgrammerPy <None>
2021-07-31 13:49:21 +0430

    Function validate_image_size ijad shod.

--- account/models.py
+++ account/models.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 from uuid import uuid4
 import os
@@ -15,6 +17,14 @@ def upload_user_pic(instance, file):
     random_str = str(uuid4()).split("-")[0]
     final_name = f"profile_pictures/{instance.username}/{random_str}{ext}"
     return final_name
+
+
+def validate_image_size(img):
+    file_size = img.size
+    limit_mb = 5
+
+    if file_size > limit_mb * 1024 * 1024:
+        raise ValidationError(_(f"File size is larger than allowed ({limit_mb} mb)."))
 
 
 # Create your models here.
