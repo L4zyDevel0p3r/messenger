be3228331bee26979955f7b5f10f92ccdddda765
MSProgrammerPy <None>
2021-07-31 22:51:01 +0430

    View register_page ijad shod.

--- account/views.py
+++ account/views.py
@@ -1,7 +1,8 @@
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
-from .forms import LoginForm
+from .forms import LoginForm, RegisterForm
+from .models import User
 
 
 # Create your views here.
@@ -30,6 +31,30 @@ def login_page(request):
             login_form.add_error("password", _("Incorrect username or password."))
 
     return render(request, "auth/login.html", context)
+
+
+def register_page(request):
+    if request.user.is_authenticated:
+        return redirect("/")
+
+    register_form = RegisterForm(request.POST or None, request.FILES or None)
+
+    context = {
+        "register_form": register_form
+    }
+
+    if register_form.is_valid():
+        username = register_form.cleaned_data.get("username")
+        password = register_form.cleaned_data.get("password")
+        picture = register_form.cleaned_data.get("picture")
+
+        user = User.objects.create_user(username=username, password=password, picture=picture)
+
+        login(request, user)
+        context["register_form"] = RegisterForm()
+        return redirect("/")
+
+    return render(request, "auth/register.html", context)
 
 
 def logout_view(request):
