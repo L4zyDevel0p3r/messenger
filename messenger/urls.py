ca84a5103d15c771f70241b443f1c143bc922900
MSProgrammerPy <None>
2021-07-31 13:06:55 +0430

    account.urls dar urlpatterns include shod.

--- messenger/urls.py
+++ messenger/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 from django.conf.urls.static import static
 from django.contrib import admin
 from messenger import settings
-from django.urls import path
+from django.urls import path, include
 from .views import home_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_page, name="HomePage")
+    path('', home_page, name="HomePage"),
+    path('', include("account.urls", namespace="Account")),
 ]
 
 if settings.DEBUG:
