29f353af12128764476790d5f3e34aea378e9091
MSProgrammerPy <None>
2021-08-01 19:25:05 +0430

    Class MessageAdmin ijad shod. Model 'Message' dar admin register shod.

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Message
+
 
 # Register your models here.
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("__str__", "group", "date")
+
+    class Meta:
+        model = Message
