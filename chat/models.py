45342b30bd3b90e3b9edfb041b0ea1283b02d22b
MSProgrammerPy <None>
2021-08-01 19:14:45 +0430

    Create model Message

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,21 @@
+from django.utils.translation import gettext_lazy as _
+from account.models import User
+from group.models import Group
 from django.db import models
 
+
 # Create your models here.
+
+
+class Message(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name=_("author"))
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, verbose_name=_("group"))
+    text = models.TextField(verbose_name=_("text"))
+    date = models.DateTimeField(auto_now_add=True, verbose_name=_("date"))
+
+    class Meta:
+        verbose_name_plural = _("messages")
+        verbose_name = _("message")
+
+    def __str__(self):
+        return self.author
