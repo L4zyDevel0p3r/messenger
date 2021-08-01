3040982cdd88b184d759dab058ccc13a32527cde
MSProgrammerPy <None>
2021-08-01 15:38:07 +0430

    Class GroupManager ijad shod.

--- group/models.py
+++ group/models.py
@@ -5,10 +5,16 @@ from django.db import models
 # Create your models here.
 
 
+class GroupManager(models.Manager):
+    pass
+
+
 class Group(models.Model):
     name = models.CharField(max_length=20, unique=True, verbose_name=_("name"))
     url_name = models.CharField(max_length=20, unique=True, verbose_name=_("url name"))
     date = models.DateTimeField(auto_now_add=True, verbose_name=_("date"))
+
+    objects = GroupManager()
 
     class Meta:
         verbose_name_plural = _("groups")
