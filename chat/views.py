479d87339a1281770c0bbb25ab83e9b7aa27bab6
MSProgrammerPy <None>
2021-08-01 18:59:02 +0430

    View room_page ijad shod.

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def room_page(request, *args, **kwargs):
+    room_name = kwargs.get("room_name")
+
+    context = {
+        "room_name": room_name
+    }
+
+    return render(request, "room.html", context)
