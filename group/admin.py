0f4bcbe9b09b8cc0dc8089bfbcf0770f9b3d6ded
MSProgrammerPy <None>
2021-08-01 17:43:51 +0430

    Field 'active' be list_display dar class GroupAdmin ezafe shod. list_editable be GroupAdmin ezafe shod.

--- group/admin.py
+++ group/admin.py
@@ -9,7 +9,8 @@ from .models import Group
 @admin.register(Group)
 class GroupAdmin(admin.ModelAdmin):
     form = GroupForm
-    list_display = ("__str__", "date")
+    list_display = ("__str__", "date", "active")
+    list_editable = ("active",)
 
     class Meta:
         model = Group
