ddf474b7e45cafc9f15eee2f775c383d283dc6b1
MSProgrammerPy <None>
2021-08-01 15:26:03 +0430

    Model 'Group' ijad shod

--- group/models.py
+++ group/models.py
@@ -1,3 +1,18 @@
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 
+
 # Create your models here.
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=20, unique=True, verbose_name=_("name"))
+    url_name = models.CharField(max_length=20, unique=True, verbose_name=_("url name"))
+    date = models.DateTimeField(auto_now_add=True, verbose_name=_("date"))
+
+    class Meta:
+        verbose_name_plural = _("groups")
+        verbose_name = _("group")
+
+    def __str__(self):
+        return self.name
