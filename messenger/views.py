57010b0a0d80ca2a429fe299a98bc42b9b5438f8
MSProgrammerPy <None>
2021-08-01 17:48:13 +0430

    list group ha (groups) be context view home_page ezafe shod.

--- messenger/views.py
+++ messenger/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from group.models import Group
 
 
 # Create your views here.
 
 def home_page(request):
+    groups = Group.objects.filter_by_active().order_by("name")
+
     context = {
+        "groups": groups
     }
 
     return render(request, "home.html", context)
