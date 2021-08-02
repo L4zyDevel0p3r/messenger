095af9130bd59a17c303a9666300ae4950c9412a
MSProgrammerPy <None>
2021-08-02 12:44:32 +0430

    View room_page modify shod. dar sorat vojod nadashtan ya 'active' nabodan 'group' (room) Http404 raise mishe.

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.http import Http404
+from group.models import Group
 
 
 # Create your views here.
@@ -7,6 +9,10 @@ from django.shortcuts import render
 @login_required(login_url="/login")
 def room_page(request, *args, **kwargs):
     room_name = kwargs.get("room_name")
+    qs = Group.objects.filter(url_name__iexact=room_name, active=True)
+
+    if not qs.exists():
+        raise Http404(f"{room_name} not found!")
 
     context = {
         "room_name": room_name
