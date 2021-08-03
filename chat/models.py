4af3c7cedbef8787700b228c92e9ee3d0deb0d1b
MSProgrammerPy <None>
2021-08-03 11:41:57 +0430

    staticmethod 'encrypt_text' va 'decrypt_text' dar class 'Message' ijad shod.

--- chat/models.py
+++ chat/models.py
@@ -1,8 +1,10 @@
 from django.utils.translation import gettext_lazy as _
+from cryptography.fernet import Fernet
 from account.models import User
 from group.models import Group
 from django.db import models
 
+# SECURITY WARNING: keep the encryption key secret!
 ENCRYPTION_KEY = b'f5cQj1_Pv68Qe-iNh1oml03pRZR4LIJllS8qFpJtTzg='
 
 
@@ -14,6 +16,24 @@ class Message(models.Model):
     group = models.ForeignKey(Group, on_delete=models.CASCADE, verbose_name=_("group"))
     text = models.TextField(verbose_name=_("text"))
     date = models.DateTimeField(auto_now_add=True, verbose_name=_("date"))
+
+    @staticmethod
+    def encrypt_text(text: str) -> str:
+        # Converting 'text' to bytes
+        text = text.encode()
+        # Creating Fernet object
+        f = Fernet(ENCRYPTION_KEY)
+        # Encrypting 'text', converting it from bytes to string and returning it
+        return (f.encrypt(text)).decode()
+
+    @staticmethod
+    def decrypt_text(text: str) -> str:
+        # Converting 'text' to bytes
+        text = text.encode()
+        # Creating Fernet object
+        f = Fernet(ENCRYPTION_KEY)
+        # Decrypting 'text', converting it from bytes to string and returning it
+        return (f.decrypt(text)).decode()
 
     class Meta:
         verbose_name_plural = _("messages")
