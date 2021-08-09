954ac7c531ea5170a24c982aa533928f56cf31eb
MSProgrammerPy <None>
2021-08-09 15:36:01 +0430

    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'messenger.settings') be balaye safhe move shod. django.setup() be bad in line ezafe shod. In kar baraye ine ke 'daphne' moghe run server error set nabodan 'DJANGO_SETTINGS_MODULE' nade.

--- messenger/asgi.py
+++ messenger/asgi.py
@@ -7,14 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
+import django
 import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'messenger.settings')
+django.setup()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from chat import routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'messenger.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
